393f59eca55f7fe1b74e1780703de1b437260098
phukon <rikiphukon16@gmail.com>
2023-11-19 11:50:14 +0530

    feat: Add parser module to compiler.js

--- tokenizer.js
+++ tokenizer.js
@@ -1,10 +1,15 @@
 const LETTERS = /[a-z]/i;
+const WHITESPACE = /\s/;
+const NUMBERS = /\d/;
+
 module.exports = function tokenizer(input) {
   const tokens = [];
   let current = 0;
+
   while (current < input.length) {
     let char = input[current];
-    if (char === '(') {
+
+    if (char === '(' || char === ')') {
       tokens.push({
         type: 'paren',
         value: '(',
@@ -12,12 +17,34 @@ module.exports = function tokenizer(input) {
       current++;
       continue;
     }
+
     if (LETTERS.test(char)) {
       let value = '';
       while (LETTERS.test(char)) {
         value += char;
         char = input[++current];
       }
+      tokens.push({
+        type: 'name',
+        value,
+      });
+      continue;
+    }
+    if (WHITESPACE.test(char)) {
+      current++;
+      continue;
+    }
+    if (NUMBERS.test(char)) {
+      let value = '';
+      while (NUMBERS.test(char)) {
+        value += char;
+        char = input[++current];
+      }
+      tokens.push({
+        type: 'number',
+        value,
+      });
+      continue;
     }
     throw new TypeError(`Unknown char: '${char}'`);
   }
